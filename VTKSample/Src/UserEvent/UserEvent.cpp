4683a31fbb04eacb6eddb552ed98da466b216644
Menghui Guan <menghui.guan@carestream.com>
2018-04-27 09:50:48 +0800

    Add UserEvent sample.

---
+++ VTKSample/Src/UserEvent/UserEvent.cpp
@@ -0,0 +1,56 @@
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL);
+VTK_MODULE_INIT(vtkInteractionStyle);
+
+#include <vtkSphereSource.h>
+#include <vtkCallbackCommand.h>
+#include <vtkCommand.h>
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+
+#include "vtkTestFilter.h"
+
+static void CallbackFunction(vtkObject *caller,
+                             long unsigned int eventId,
+                             void *clientData,
+                             void *callData );
+
+int main(int, char *[]) {
+  // Create a renderer, render window, and interactor
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  vtkSmartPointer<vtkTestFilter> filter =
+    vtkSmartPointer<vtkTestFilter>::New();
+
+  vtkSmartPointer<vtkCallbackCommand> callback =
+    vtkSmartPointer<vtkCallbackCommand>::New();
+  callback->SetCallback(CallbackFunction );
+  filter->AddObserver(filter->RefreshEvent, callback);
+
+  filter->Update();
+
+  filter->InvokeEvent(filter->RefreshEvent, NULL);
+
+  renderWindow->Render();
+  renderWindowInteractor->Start();
+
+  return EXIT_SUCCESS;
+}
+
+void CallbackFunction(vtkObject *caller,
+                      long unsigned int eventId,
+                      void *clientData, void *callData ) {
+  std::cout << "CallbackFunction called." << std::endl;
+}
