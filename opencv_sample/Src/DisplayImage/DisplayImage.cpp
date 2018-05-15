693b211dd25f06807dfed0f3f21c8f32c5b3bf9d
Menghui Guan <menghui.guan@carestream.com>
2018-05-15 10:49:34 +0800

    use vld

--- opencv_sample/Src/DisplayImage/DisplayImage.cpp
+++ opencv_sample/Src/DisplayImage/DisplayImage.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include "vld.h"
 using namespace cv;
 int main(int argc, char **argv) {
+  //int *p = new int[4];
+  CvMat *mat = cvCreateMat(4, 4, 0);
+  cvReleaseMat(&mat);
+
+  //cv::Mat *p = new cv::Mat();
+  //CvMat *p2 = new CvMat;
   if (argc != 2) {
     printf("usage: DisplayImage.out <Image_Path>\n");
     return -1;
