757ab9e2c4695d7276cfca038f13756c77a3cce4
Menghui Guan <menghui.guan@carestream.com>
2019-07-12 11:06:06 +0800

    fill version in cmake.

---
+++ Cmake_sln_sample/versioninfo.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/************************************************************************/
+/* Set the version numbers and module information manually here:                                        */
+/************************************************************************/
+
+#define MODULE_MAJOR_VERSION			1
+#define MODULE_MINOR_VERSION			2
+#define MODULE_REVISION_VERSION	    	3
+#define MODULE_BUILD_NUMBER             544
+
+
+
