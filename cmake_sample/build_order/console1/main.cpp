0e020ad2d63ada93adc93793596adcbf650fbdd9
Menghui.Guan <menghui.guan@csdental.com>
2021-07-02 16:32:05 +0800

    use CMAKE_CXX_FLAGS to disable some warnings.

--- cmake_sample/build_order/console1/main.cpp
+++ cmake_sample/build_order/console1/main.cpp
@@ -1,8 +1,20 @@
 #include "lib1.h"
 #include <stdio.h>
 #include "dll1.h"
+#include <vector>
+
+void disable_warning() {
+  std::vector<int>v;
+  int n = v.size();//warning C4267
+}
+void f1(void) throw(int) {}   // C4290
+void input() {
+  int n;
+  scanf("%d", &n);//warning C4996
+}
 int main() {
   print(1);
   TestOpenMP(8);
+
   return 0;
 }
